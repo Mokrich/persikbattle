a52eb7183d68d4dd69ebdaa56c1542a2da2e854b
Egor Makarov <egorgubaha333@gmail.com>
2026-02-17 00:10:08 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ export default function App() {
 
   // ===== LOAD TASKS =====
   const loadTasks = async () => {
-    const { data, error } = await supabase.from("Zadanie").select("*");
+    const { data, error } = await supabase.from("tasks").select("*");
 
     console.log("TASKS FROM DB:", data);
     console.log("ERROR:", error);
